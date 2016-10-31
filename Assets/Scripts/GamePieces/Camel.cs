c8c750eb2d8a749acbd15b46f838bbee5bc97b89
Matt Vroman <matt@matthewvroman.com>
2016-10-30 23:24:58 -0700

    Add prequel chess pieces

---
+++ Assets/Scripts/GamePieces/Camel.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class Camel : GamePiece {
+
+	public override List<GridCell>GetMovementCells(GameGrid grid, GridCell cell)
+	{
+		List<GridCell>cells = grid.GetCellsForDirection(cell, 3, 1, 1);
+		cells.AddRange(grid.GetCellsForDirection(cell, 3, -1, 1));
+		cells.AddRange(grid.GetCellsForDirection(cell, -3, -1, 1));
+		cells.AddRange(grid.GetCellsForDirection(cell, -3, 1, 1));
+		cells.AddRange(grid.GetCellsForDirection(cell, 1, 3, 1));
+		cells.AddRange(grid.GetCellsForDirection(cell, -1, 3, 1));
+		cells.AddRange(grid.GetCellsForDirection(cell, -1, -3, 1));
+		cells.AddRange(grid.GetCellsForDirection(cell, 1, -3, 1));
+		return cells;
+	}
+
+	public override List<GridCell>GetAttackCells(GameGrid grid, GridCell cell)
+	{
+		return GetMovementCells(grid, cell);
+	}
+}
---
+++ Assets/Scripts/GamePieces/Princess.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class Princess : GamePiece {
+
+	public override List<GridCell>GetMovementCells(GameGrid grid, GridCell cell)
+	{
+		List<GridCell>cells = grid.GetCellsForDirection(cell, 2, 1, 1);
+		cells.AddRange(grid.GetCellsForDirection(cell, 2, -1, 1));
+		cells.AddRange(grid.GetCellsForDirection(cell, -2, -1, 1));
+		cells.AddRange(grid.GetCellsForDirection(cell, -2, 1, 1));
+		cells.AddRange(grid.GetCellsForDirection(cell, 1, 2, 1));
+		cells.AddRange(grid.GetCellsForDirection(cell, -1, 2, 1));
+		cells.AddRange(grid.GetCellsForDirection(cell, -1, -2, 1));
+		cells.AddRange(grid.GetCellsForDirection(cell, 1, -2, 1));
+		cells.AddRange(grid.GetDiagonalCells(cell));
+
+		return cells;
+	}
+
+	public override List<GridCell>GetAttackCells(GameGrid grid, GridCell cell)
+	{
+		return GetMovementCells(grid, cell);
+	}
+}
