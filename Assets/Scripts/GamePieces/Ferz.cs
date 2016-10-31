f3da5161b138d65074a131302fa5e0d7cef3427d
Matt Vroman <matt@matthewvroman.com>
2016-10-30 23:43:01 -0700

    Add 2 popular fairy chess pieces

---
+++ Assets/Scripts/GamePieces/Ferz.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class Ferz : GamePiece {
+
+	public override List<GridCell>GetMovementCells(GameGrid grid, GridCell cell)
+	{
+		return grid.GetDiagonalCells(cell, 1);
+	}
+
+	public override List<GridCell>GetAttackCells(GameGrid grid, GridCell cell)
+	{
+		return GetMovementCells(grid, cell);
+	}
+}
---
+++ Assets/Scripts/GamePieces/Wazir.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class Wazir : GamePiece {
+
+	public override List<GridCell>GetMovementCells(GameGrid grid, GridCell cell)
+	{
+		return grid.GetParallelCells(cell, 1);
+	}
+
+	public override List<GridCell>GetAttackCells(GameGrid grid, GridCell cell)
+	{
+		return GetMovementCells(grid, cell);
+	}
+}
